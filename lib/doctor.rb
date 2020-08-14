68f57e6c5e687f08dfda420a350bc1caa89ecc44
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-14 06:25:12 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
+  end
+
+  def appointments
+    Appointment.all.select do |appt|
+      appt.doctor == self
+    end
+  end
+
+  def patients
+    appointments.collect do |appt|
+      appt.patients
+    end
+  end
+end
+
+  
