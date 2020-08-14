837d95c0ec0b1fe656a7334613d0968532a83a4d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-14 06:07:29 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,5 +12,15 @@ class Genre
     @@all
   end
 
+  def songs
+    Song.select do |song|
+      song.genre == self
+    end
+  end
 
-  
+  def artists
+    songs.collect do |song|
+      song.artist
+    end
+  end
+end
